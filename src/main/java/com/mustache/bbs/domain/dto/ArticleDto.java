d9542a3811fbdb537ecbe737b010b8f7eb524075
soonmineom <skdlfma123@gmail.com>
2022-11-07 15:28:59 +0900

    ArticleDto로 받은 정보를, toEntity() 메소드로 JPA객체인 Article로 변환

---
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -0,0 +1,22 @@
+package com.mustache.bbs.domain.dto;
+
+import com.mustache.bbs.domain.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ArticleDto {
+    private Long id;
+    private String title;
+    private String content;
+
+    public Article toEntity() {
+        return new Article (getTitle(), getContent());
+    }
+
+}
