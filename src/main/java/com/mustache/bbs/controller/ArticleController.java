bbad36c4481647c2d2887efff774926be9cc9b80
soonmineom <skdlfma123@gmail.com>
2022-11-07 15:54:35 +0900

    Repository(Dao 역할)을 만들고, DB에 데이터 넣어보기

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -2,7 +2,9 @@ package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class ArticleController {
 
+//    @Autowired
+//    private ArticleRepository articleRepository;
+
+    // Autowired 써도 되지만 요즘에는 아래 방식을 더 많이 사용
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
@@ -22,6 +35,7 @@ public class ArticleController {
     public String createArticle(ArticleDto form)  {
         log.info(form.toString());
         Article article = form.toEntity();
+        articleRepository.save(article);
         return "";
     }
 }
