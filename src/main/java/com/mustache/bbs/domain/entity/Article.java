072a8f1dbfbf4a42ccc5dd5a39132eb49fcbb720
soonmineom <skdlfma123@gmail.com>
2022-11-07 15:27:46 +0900

    JPA 객체가 될 Article 객체 선언

---
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -0,0 +1,27 @@
+package com.mustache.bbs.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity //JPA에서 객체로 다루겠다는 의미
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Article {
+
+    @Id //PrimaryKey 역할
+    @GeneratedValue // AutoIncrease 기능 (id를 직접 입력할 경우는 제외한다)
+    private Long id;
+    private String title;
+    private String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
